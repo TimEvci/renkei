7093e140a51beba781b6ca18a31addb7a693e18c
TimEvci <tevci93@gmail.com>
2024-05-25 02:12:51 +0300

    chore: update default styles

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,6 +8,7 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#1E1E1E" />
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
